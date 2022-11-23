a238a447c468c8b1076da3b826b09ad64228ad32
Nacho Divasson <nacho@divasson.com>
2022-11-23 19:21:32 +0100

    linkar funciones a dash

--- Utilities/leer/readFileFinal_y_Geos.py
+++ Utilities/leer/readFileFinal_y_Geos.py
@@ -2,21 +2,22 @@
 import pandas as pd
 import numpy as np
 import json
+import sys
 
 def leerFicheroFinal():
-    """_summary_
+    """Devuelve el fichero bueno
 
     Returns:
-        _type_: _description_
+        DF: DF final
     """  
     z = pd.read_parquet("../../Data/total data/modified data/listingsFinalConRentabilidad.parquet")
     return z
 
 def leerFicherosGeo():
-    """_summary_
+    """Devuelve una tupla de los ficheros geo
 
     Returns:
-        _type_: _description_
+        (geodataframe,geodataframe): Los dos GeoDF
     """  
     jsonGeoNeigh = json.load(open("../../Data/neighbourhoods/neighbourhoods.geojson"))
     bigJSONNeigh = json.load(open("../../Data/neighbourhoods/bigneighbourhoods.geojson"))
@@ -24,18 +25,35 @@ def leerFicherosGeo():
 
 
 def opcionesGlobales():
+    """Devuelve parámetros totales de la app
+
+    Returns:
+        dict: diccionario a través del cual se acceden a varios valores globales
+    """
     z = {
-            "Barrios":{
+            "Colores Barrios":{
                 "Brooklyn": "#1f77b4",
                 "Bronx": "#ff7f0e",
                 "Staten Island":"#2ca02c",
                 "Queens":"#9467bd",
                 "Manhattan":"#d62728"
             },
-            "HabitacionesTipo":["Entire rental unit", "Private room in rental unit", "Private room in home", "Entire condo", "Entire home"]
+            "HabitacionesTipo":["Entire rental unit", "Private room in rental unit", "Private room in home", "Entire condo", "Entire home"],
+            "Opciones desplegable Vivienda":["property_type","room_type","beds","baths","review_scores_rating"],
+            "Opciones desplegable Finanzas":["price","occupancy_rate","revenue","price per unit","profitability"],
+            "Barrios":{
+                "Brooklyn": "#1f77b4",
+                "Bronx": "#ff7f0e",
+                "Staten Island":"#2ca02c",
+                "Queens":"#9467bd",
+                "Manhattan":"#d62728"
+            }
         }
     return z
 
 if __name__=="__main__":
-    pass
+    
+
+    for p in sys.path:
+        print( p )
     
