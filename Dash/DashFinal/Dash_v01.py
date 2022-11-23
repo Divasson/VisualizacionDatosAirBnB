b73a4ce17aa934f4a054de864d056cef55cd1b35
Nacho Divasson <nacho@divasson.com>
2022-11-23 22:44:39 +0100

    dash_v1

--- Dash/DashFinal/Dash_v01.py
+++ Dash/DashFinal/Dash_v01.py
@@ -1,18 +1,22 @@
 # Importamos las librerias mínimas necesarias
-import pandas as pd
-import numpy as np
-import plotly.graph_objects as go
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output, State
-import logging
-from plotly.subplots import make_subplots
-import sys
-import os
 import importlib.machinery
 import importlib.util
+import logging
+import os
+import sys
 from pathlib import Path
+import dash
+
+import dash_bootstrap_components as dbc
+from dash import dcc
+from dash import html
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+from dash.dependencies import Input, Output, State
+from plotly.subplots import make_subplots
+
+
 
 # Get path to mymodule. Credits to = https://csatlas.com/python-import-file-module/
 script_dir = Path( __file__ ).parent.parent
@@ -32,323 +36,107 @@ listings_filtered_df = mymodule.leerFicheroFinal()
 (jsonGeoNeigh,bigJSONNeigh) = mymodule.leerFicherosGeo()
 opcionesGlobales = mymodule.opcionesGlobales()
 
+data = []
+
+for x in listings_filtered_df["neighbourhood_group_cleansed"].unique():
+    data.append(go.Histogram(
+                    x = listings_filtered_df[listings_filtered_df["neighbourhood_group_cleansed"] == x]['price'],
+                    marker_color=opcionesGlobales["Colores Barrios"][x],
+                    xbins=dict(
+                        start= 0,
+                        end= 600,
+                        size=5
+                    ),
+                    opacity=0.5,
+                    name = x
+                )
+    )
+    
+layout = go.Layout(title = "Distribución de los precios por barrios", xaxis_title = "Precios por noche", yaxis_title = "Frecuencia",
+                   barmode = "overlay", bargap = 0.1)
+
+fig = go.Figure(data = data, layout = layout)
 
 
-app = dash.Dash()
+
+app = dash.Dash(external_stylesheets=[dbc.themes.SLATE])
 
 logging.getLogger('werkzeug').setLevel(logging.INFO)
+dash.register_page(__name__, path='/')
 
 
 
-app.layout = html.Div(
-    children= [
-        html.H1( # Primera fila
-            children = [
-                "Análisis sobre el mercado de AirBnB en Nueva York"
+app.layout = dbc.Container(
+    [
+        dcc.Store(id="store"), # para guardar informacion. Es una variable
+        html.H1("Estudio sobre los AirBnBs en NYC"),
+        html.Hr(), # Cambio de tercio
+        dbc.Tabs(
+            [
+                dbc.Tab(label="Rentabilidad", tab_id="profitability"),
+                dbc.Tab(label="Descriptivo", tab_id="descriptive"),
             ],
-            id = "titulo",
-            style = {
-                "text-align": "center",
-                "vertical-align":"middle",
-                #"text-decoration": "underline",
-                "backgroundColor": "#2c3e50",
-                "color":"white",
-                #"margin-bottom": "20px",
-                "border-style": "outset",
-                #"border-color": "lightblue",
-                "height": "70px"
-            }
+            id="tabs",
+            active_tab="profitability",
         ),
-
-        html.H2( # Segunda fila
-            children = [
-                "1) Comparativa de notas entre asignaturas y tipo de etnia"
+        html.Div(
+            children=[
+                dcc.Graph(figure=fig)
             ],
-            id = "subtitulo1",
-            style ={
-                "text-align": "left",
-                "display": "block"
-            }
-        ),
-
-        html.Div( # Tercera fila
-            children = [
-                html.Div( # Bloque izquierdo
-                    children = [
-                        html.H3(
-                            children = [
-                                "Primer grupo a comparar"
-                            ],
-                            id = "primer_grupo",
-                            style = {
-                                "display": "block",
-                                "text-align": "center"
-                            }
-                        ),
-                        dcc.Dropdown(
-                            options = "options_dropdown_race",
-                            placeholder = "Selecciona una raza",
-                            id = "dropdown_race",
-                            style = {
-                                "display": "block",
-                                "width": "300px",
-                                "margin-left": "10px"
-                            }
-                        ),
-                        dcc.Dropdown(
-                            options = "options_dropdown_subjects",
-                            placeholder = "Selecciona una asignatura",
-                            id = "dropdown_subject",
-                            style = {
-                                "display": "block",
-                                "width": "300px",
-                                "margin-left": "10px"
-                            }
-                        ),
-                        dcc.Graph(
-                            id = "dropdown_figure",
-                            style = {
-                                "display": "none"
-                            }
-                        )
-                    ],
-                    style = {
-                        "width": "700px",
-                        "height": "600px",
-                        "display": "inline-block",
-                        "border-style": "ridge",
-                        "border-color": "black"
-                    }, 
-                ),
-
-                html.Div( # Bloque derecho
-                    children = [
-                        html.H3(
-                            children = [
-                                "Segundo grupo a comparar"
-                            ],
-                            id = "segundo_grupo",
-                            style = {
-                                "display": "block",
-                                "text-align": "center"
-                            }
-                        ),
-                        dcc.Dropdown(
-                            options = "options_dropdown_race",
-                            placeholder = "Selecciona una raza",
-                            id = "dropdown_race_2",
-                            style = {
-                                "display": "block",
-                                "width": "300px",
-                                "margin-left": "10px"
-                            }
-                        ),
-                        dcc.Dropdown(
-                            options = "options_dropdown_subjects",
-                            placeholder = "Selecciona una asignatura",
-                            id = "dropdown_subject_2",
-                            style = {
-                                "display": "block",
-                                "width": "300px",
-                                "margin-left": "10px"
-                            }
-                        ),
-                        dcc.Graph(
-                            id = "dropdown_figure_2",
-                            style = {
-                                "display": "none"
-                            }
-                        )
-                    ],
-                    style = {
-                        "width": "700px",
-                        "height": "600px",
-                        "display": "inline-block",
-                        "margin-left": "20px",
-                        "border-style": "ridge",
-                        "border-color": "black"
-                    },
-                ) 
-            ]
-        ),
-
-        html.Div( # Cuarta fila
-            children = [
-                html.H2(
-                    children = [
-                        "2) Visualizar proporciones de variables categóricas"
-                    ],
-                    id = "titulo_cuarta_fila",
-                    style ={
-                        "text-align": "left",
-                        "display": "block"
-                    }
-                ),
-
-                html.Div(
-                    children = [
-                        dcc.Checklist(
-                            options = "options_checklist",
-                            labelStyle = {
-                                'display': 'inline-block',
-                                'font-size': "18px",
-                                'margin-right': "10px"
-                            },
-                            id = "checklist_cat",
-                            style = {
-                                "display": "inline-block",
-                            }
-                        ),
-                        html.Button(
-                            children = [
-                                "Mostrar"
-                            ],
-                            id = "boton_cat",
-                            title = "Mostrar",
-                            n_clicks = 0,
-                            style = {
-                                'background-color': 'lightgrey',
-                                'color': 'steelblue',
-                                'height': '35px',
-                                'width': '100px',
-                                'margin-left': '50px',
-                                'border-radius': "5px"
-                            }
-                        )
-                    ],
-                    style = {
-                        "display": "block"
-                    }
-                ),
-                dcc.Graph(
-                    id = "pie_charts",
-                    style = {
-                        "display" : "none"
-                    }
-                )
-            ],
-            id = "cuarta_fila",
-        )
-    ],
-    style = {
-        "font-family": "Arial"
-    }
+            id="tab-content", 
+            className="p-4"
+        ) 
+    ]
 )
-""" 
-@app.callback(
-    Output("dropdown_figure", "figure"),
-    Output("dropdown_figure", "style"),
-    Input("dropdown_race", "value"),
-    Input("dropdown_subject",'value')
-)
-def figure_dropdown(dropdown_race_value,dropdown_subject_value):
-
-    subject_dict = {
-        "math score": "Math",
-        "reading score": "Reading",
-        "writing score": "Writing"
-    }
-
-    if dropdown_race_value and dropdown_subject_value:
-        fig = go.Figure() 
-        fig.add_trace(
-            go.Histogram(
-                x = df[df["race/ethnicity"] == dropdown_race_value][dropdown_subject_value],
-                xbins=dict(
-                    start= 0,
-                    end= 100,
-                    size=5,
-                ),
-                marker_color = "firebrick"
-            )
-        )
-        fig.update_layout(
-            title = f"Notas obtenidas en la asignatura {subject_dict[dropdown_subject_value]} de la raza {dropdown_race_value}", 
-            xaxis_title = "Puntuación", 
-            yaxis_title = "Frecuencia", 
-            bargap = 0.1
-        )
-        return (fig,{"display":"block"})
-    else:
-        return (go.Figure(data = [], layout = {}), {"display": "none"})
 
 @app.callback(
-    Output("dropdown_figure_2", "figure"),
-    Output("dropdown_figure_2", "style"),
-    Input("dropdown_race_2", "value"),
-    Input("dropdown_subject_2",'value')
+    Output("tab-content", "children"),
+    [Input("tabs", "active_tab"), Input("store", "data")],
 )
-def figure_dropdown_2(dropdown_race_value,dropdown_subject_value):
-
-    subject_dict = {
-        "math score": "Math",
-        "reading score": "Reading",
-        "writing score": "Writing"
-    }
-
-    if dropdown_race_value and dropdown_subject_value:
-        fig = go.Figure() 
-        fig.add_trace(
-            go.Histogram(
-                x = df[df["race/ethnicity"] == dropdown_race_value][dropdown_subject_value],
-                xbins=dict(
-                    start= 0,
-                    end= 100,
-                    size=5,
-                ),
-                marker_color = "steelblue"
+def render_tab_content(active_tab, data):
+    """
+    This callback takes the 'active_tab' property as input, as well as the
+    stored graphs, and renders the tab content depending on what the value of
+    'active_tab' is.
+    """
+    if active_tab and data is not None:
+        if active_tab == "profitability":
+            return dcc.Graph(figure=fig)
+        elif active_tab == "descriptive":
+            return dbc.Row(
+                [
+                    dbc.Col(dcc.Graph(figure=fig), width=6),
+                    dbc.Col(dcc.Graph(figure=fig), width=6),
+                ]
             )
-        )
-        fig.update_layout(
-            title = f"Notas obtenidas en la asignatura {subject_dict[dropdown_subject_value]} de la raza {dropdown_race_value}", 
-            xaxis_title = "Puntuación", 
-            yaxis_title = "Frecuencia", 
-            bargap = 0.1
-        )
-        return (fig,{"display":"block"})
-    else:
-        return (go.Figure(data = [], layout = {}), {"display": "none"})
+    return fig
 
-@app.callback(
-    Output("pie_charts", "figure"),
-    Output("pie_charts", "style"),
-    Input("boton_cat", "n_clicks"),
-    State("checklist_cat", "value"),
-)
-def checklist_callback(n_clicks,checklist_value):
-    print(checklist_value)
+""" @app.callback(Output("store", "data"),[Input("button"),Input("n_clicks")])
+def generate_graphs(n):
+    '''
+    This callback generates three simple graphs from random data.
+    '''
+    if not n:
+        # generate empty graphs when app loads
+        return fig
 
-    if (checklist_value is None) or (checklist_value == []):
-        return (go.Figure(data = [], layout = {}), {"display":"none"})
-    else:
-        print(checklist_value)
-        fig = make_subplots(
-            rows=1, 
-            cols=len(checklist_value),
-            specs = [[{'type':'domain'}]*len(checklist_value)]
-        )
+    # simulate expensive graph generation process
+    #time.sleep(2)
 
-        for col in checklist_value:
-            level_count = pd.DataFrame(df[col].value_counts()).reset_index().rename(columns = {"index": col, col: "count"})
-            fig.add_trace(
-                go.Pie(
-                    labels = level_count[col],
-                    values = level_count["count"],
-                    textinfo = 'label+percent',
-                    insidetextorientation = 'radial',
-                    textposition = 'inside',
-                    sort = False,
-                    showlegend = False,
-                    hole = 0.5,
-                    name = col
-                ),
-                row = 1,
-                col = checklist_value.index(col) + 1
-            )
-        
-        fig.update_traces(textfont_size=12)
-        return (fig,{"display": "block"}) """
-        
+    # generate 100 multivariate normal samples
+    data = np.random.multivariate_normal([0, 0], [[1, 0.5], [0.5, 1]], 100)
 
-if __name__ == '__main__':
-    app.run_server()
+    scatter = go.Figure(
+        data=[go.Scatter(x=data[:, 0], y=data[:, 1], mode="markers")]
+    )
+    hist_1 = go.Figure(data=[go.Histogram(x=data[:, 0])])
+    hist_2 = go.Figure(data=[go.Histogram(x=data[:, 1])]) 
+
+    # save figures in a dictionary for sending to the dcc.Store
+    return fig
+ """
+
+if __name__ == "__main__":
+    app.run_server(debug=True, port=8888)
+
+
