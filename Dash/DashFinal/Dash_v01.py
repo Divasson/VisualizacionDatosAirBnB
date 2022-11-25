e6a7df17c4353c113ff8e9f647a447b5a973d82b
Nacho Divasson <nacho@divasson.com>
2022-11-25 13:32:18 +0100

    Dash_v02

--- Dash/DashFinal/Dash_v01.py
+++ Dash/DashFinal/Dash_v01.py
@@ -1,3 +1,11 @@
+
+## *: Importante: Belen es espectacular
+## TODO: Margenes
+## TODO: Centrado pagina web
+## TODO: Zoom y centrado plot
+
+
+
 # Importamos las librerias mínimas necesarias
 import importlib.machinery
 import importlib.util
@@ -16,7 +24,8 @@ import plotly.graph_objects as go
 from dash.dependencies import Input, Output, State
 from plotly.subplots import make_subplots
 
-
+import math
+import plotly.express as px
 from PIL import Image
 
 
@@ -50,28 +59,52 @@ loader.exec_module( mymodule )
 listings_filtered_df = mymodule.leerFicheroFinal()
 (jsonGeoNeigh,bigJSONNeigh) = mymodule.leerFicherosGeo()
 opcionesGlobales = mymodule.opcionesGlobales()
-""" 
-data = []
 
-for x in listings_filtered_df["neighbourhood_group_cleansed"].unique():
-    data.append(go.Histogram(
-                    x = listings_filtered_df[listings_filtered_df["neighbourhood_group_cleansed"] == x]['price'],
-                    marker_color=opcionesGlobales["Colores Barrios"][x],
-                    xbins=dict(
-                        start= 0,
-                        end= 600,
-                        size=5
-                    ),
-                    opacity=0.5,
-                    name = x
-                )
-    )
-    
-layout = go.Layout(title = "Distribución de los precios por barrios", xaxis_title = "Precios por noche", yaxis_title = "Frecuencia",
-                   barmode = "overlay", bargap = 0.1)
 
-fig = go.Figure(data = data, layout = layout)
- """
+
+figuraRentabilidadDistritos = go.Figure()
+figuraRentabilidadDistritos.add_trace(trace=go.Choroplethmapbox(
+                                                geojson=jsonGeoNeigh,
+                                                featureidkey='properties.neighbourhood',
+                                                locations=listings_filtered_df.groupby("neighbourhood_cleansed",as_index=False).agg("mean")['neighbourhood_cleansed'],
+                                                z=listings_filtered_df.groupby("neighbourhood_cleansed",as_index=False).agg("mean")['profitability'],
+                                                colorscale=px.colors.sequential.YlGnBu,
+                                                #colorscale=px.colors.diverging.balance,
+                                                #zmin=zmin,zmax=zmax,
+                                                colorbar=dict(thickness=10, ticklen=1,title="%",tickformat='1%',tickcolor='grey',tickfont=dict(size=14, color='grey'),titlefont=dict(color='grey')),
+                                                text=listings_filtered_df.groupby("neighbourhood_cleansed",as_index=False).agg("mean")['neighbourhood_cleansed'],
+                                                hovertemplate = "<b>%{text}</b><br>" +
+                                                                "Avg. return: %{z:.0%}<br>" +
+                                                                "<extra></extra>"
+                                            )
+                                      )
+figuraRentabilidadDistritos.update_layout(mapbox1=dict(zoom=8.5,style='carto-positron',center={"lat": 40.7, "lon": -74}))
+#figuraRentabilidadDistritos.data[0].colorbar.x=-0.1
+figuraRentabilidadDistritos.update_layout(height=300,width=400,margin=dict(t=0,b=0,l=0,r=0),title="Rentabilidad (%) media",
+                                          paper_bgcolor='rgba(0,0,0,0)',
+                                          plot_bgcolor='rgba(0,0,0,0)')    
+
+figuraPrecioDistritos = go.Figure()
+figuraPrecioDistritos.add_trace(trace=go.Choroplethmapbox(
+                                                geojson=jsonGeoNeigh,
+                                                featureidkey='properties.neighbourhood',
+                                                locations=listings_filtered_df.groupby("neighbourhood_cleansed",as_index=False).agg("mean")['neighbourhood_cleansed'],
+                                                z=listings_filtered_df.groupby("neighbourhood_cleansed",as_index=False).agg("mean")['price'],
+                                                colorscale=px.colors.sequential.YlGnBu,
+                                                #colorscale=px.colors.diverging.balance,
+                                                #zmin=zmin,zmax=zmax,
+                                                colorbar=dict(thickness=10, ticklen=1,title="$",tickformat='1$',tickcolor='grey',tickfont=dict(size=14, color='grey'),titlefont=dict(color='grey')),
+                                                text=listings_filtered_df.groupby("neighbourhood_cleansed",as_index=False).agg("mean")['neighbourhood_cleansed'],
+                                                hovertemplate = "<b>%{text}</b><br>" +
+                                                                "Avg. price: %{z:.0$}<br>" +
+                                                                "<extra></extra>"
+                                            )
+                                      )
+figuraPrecioDistritos.update_layout(mapbox1=dict(zoom=8.5,style='carto-positron',center={"lat": 40.7, "lon": -74}))
+figuraPrecioDistritos.update_layout(height=300,width=400,margin=dict(t=0,b=0,l=0,r=0),title="Precio ($) media",
+                                          paper_bgcolor='rgba(0,0,0,0)',
+                                          plot_bgcolor='rgba(0,0,0,0)')    
+ 
 
 
 app = dash.Dash(external_stylesheets=[dbc.themes.SLATE])
@@ -116,7 +149,7 @@ app.layout = dbc.Container(
     [
         #dcc.Store(id="store"), # para guardar informacion. Es una variable
         
-        dbc.Container([
+        dbc.Row([
                     html.Img(src=returnImage('\Images\AirBnB\logoRojo.png'),
                              style={
                                  "display":"inline-block",
@@ -134,50 +167,136 @@ app.layout = dbc.Container(
                 style ={
                     "align": "left",
                     'display':'inline-block',
-                    "padding-top":"2%"
+                    "padding-top":"2%",
+                    "width":"100%",
+                    #"margin":"5%"
                 }
         ),
         html.Hr(), # Cambio de tercio
         dbc.Row([
             dbc.Col([
-                    html.H3("Filtros",
-                            style={
-                                    "display":"block",
-                                    "vertical-align": "bottom"
-                                }),
-                    html.Hr(), # Cambio de tercio
-                    dbc.Label("Filtro rentabilidad", html_for="range-slider"),
-                    dcc.RangeSlider(id="range-slider", min=0, max=20, value=[0, 3.5],
-                                    
-                                    #marks={i: '{}'.format(1.5 ** i) for i in range(4)},
-                                    marks={
-                                        0: '0%',
-                                        5: '5%',
-                                        10: '10%',
-                                        20: '20%',
-                                        },
-                                    #type="percentage",
-                                    tooltip={"placement": "bottom", "always_visible": True},
+                    dbc.Row([
+                            html.H3("Rentabilidad (%) por distrito",
+                                    style={
+                                        "size":18,
+                                        "color":"grey",
+                                        "padding-bottom":"2%"
+                                    },
                                     ),
-                    html.Br(),
-                    dbc.Col([
-                            dbc.DropdownMenu(itemsDropDownBarrios,label="Barrios",
-                                            color="info",style={'display':'inline-block'}),
-                            html.Label("Seleccionado: Todos",style={'display':'inline-block'})
-                            ],
-                            id="seleccionBarrios",
-                            style={
-                                'display':'inline-block',
-                                }),
+                            dcc.Graph(figure=figuraRentabilidadDistritos)
+                        ],
+                            id="plt-profitability"
+                    ),
+                    dbc.Row([
+                            html.H3("Precio ($) por distrito",
+                                    style={
+                                        "size":18,
+                                        "color":"grey",
+                                        "padding-bottom":"2%",
+                                        "padding-top":"2%"
+                                    },
+                                    ),
+                            dcc.Graph(figure=figuraPrecioDistritos)
+                        ],
+                            id="plt-price"
+                    ),
+                    dbc.Row([
+                        html.H3("Filtros",
+                                style={
+                                        "display":"block",
+                                        "vertical-align": "bottom",
+                                        "padding-top":"1%"
+                                    }),
+                        html.Hr(), # Cambio de tercio
+                        dbc.Label("Filtro rentabilidad (%)", html_for="range-slider"),
+                        dcc.RangeSlider(id="range-slider-rentabilidad", min=0, max=20, value=[0, 3.5],
+                                        
+                                        #marks={i: '{}'.format((math.sqrt(i))) for i in [0.1,1,3,5,20]},
+                                        marks={
+                                            0: '0%',
+                                            5: '5%',
+                                            10: '10%',
+                                            20: '20%',
+                                            },
+                                        
+                                        tooltip={"placement": "bottom", "always_visible": True},
+                                        
+                                        ),
+                        html.Br(),
+                        dbc.Label("Filtro Precio/noche ($)", html_for="range-slider",style={"padding-top":"1.5%"}),
+                        dcc.RangeSlider(id="range-slider-precio", min=0, max=1300, value=[0, 400],
+                                        
+                                        #marks={i: '{}'.format((math.sqrt(i))) for i in [0.1,1,3,5,20]},
+                                        marks={
+                                            0: '0$',
+                                            100: '100$',
+                                            200: '200$',
+                                            500: '500$',
+                                            1300: '1300$',
+                                            },
+                                        
+                                        tooltip={"placement": "bottom", "always_visible": True},
+                                        
+                                        ),
+                        
+                        
+                    ]),
+                    dbc.Row([
+                        html.Br(),
+                        dbc.Col([dcc.Dropdown(
+                                    id="barrios-seleccion",
+                                    options=[
+                                        "Todos",
+                                        "Manhattan",
+                                        "Brooklyn",
+                                        "Queens",
+                                        "Staten Island",
+                                        "Bronx"
+                                    ],
+                                    value="Todos",
+                                    style={
+                                        "width":"80%",
+                                        "padding-top":"3%"
+                                    }
+                                    ),
+                                ],
+                                id="seleccionBarrios",
+                                style={
+                                    'display':'inline-block',
+                                },
+                                width=8
+                        ),
+                        dbc.Col([html.Div(
+                                        [
+                                            dbc.Checklist(
+                                                options=[
+                                                    {"label": "Aplicar Filtros", "value": 1},
+                                                ],
+                                                value=[0],
+                                                id="switches-input",
+                                                switch=True,
+                                                style={
+                                                    "vertical-align":"middle",
+                                                    "padding-top":"3%",
+                                                    #"background":"grey",
+                                                },
+                                                input_checked_style={
+                                                    "background-color":"#bada55"
+                                                    
+                                                },
+                                            ),
+                                        ]
+                                    )
+                        ],
+                                width=4)
+                    ]),
                     
-                    ],
-                    id="Menu izq",
-                    width=3
-                ),
+            ]),
             dbc.Col([
                     dbc.Tabs([
                                 dbc.Tab(label="Rentabilidad", tab_id="profitability"),
                                 dbc.Tab(label="Descriptivo", tab_id="descriptive"),
+                                dbc.Tab(label="Bonus", tab_id="bonus"),
                             ],
                             id="tabs",
                             active_tab="profitability",
@@ -186,8 +305,74 @@ app.layout = dbc.Container(
                     )
             
         ])
-    ]
+    ],
+    style={
+        "width":"100%",
+        #"margin":"3%",
+        #"align":"center"
+    }
 )
+
+
+
+def filtrarDF(rentabilidadMin,rentabilidadMax,barrio,precioMin,precioMax):
+    """Devuelve DF filtrado con los posibles filtros existentes
+
+    Args:
+        rentabilidadMin (float): _description_
+        rentabilidadMax (float): _description_
+        barrio (str): Barrio o Todo
+        precioMin (float): _description_
+        precioMax (float): _description_
+
+    Returns:
+        DF: DF filtrado
+    """    
+    df = listings_filtered_df
+    if barrio!="Todos":
+        df = df[df["neighbourhood_group_cleansed"]==barrio]
+    z = df[
+           ((df["profitability"]>=rentabilidadMin)&(df["profitability"]<=rentabilidadMax))
+           &
+           ((df["price"]>=precioMin)&(df["profitability"]<=precioMax))
+        ]
+    return z
+    
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
